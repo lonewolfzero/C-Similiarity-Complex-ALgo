42f34ddb2451fd27d9eeba26548ab5a5c55bd9c8
Digitalman <56293869+digitalman51@users.noreply.github.com>
2023-11-01 00:52:21 +0700

    First Commit
    First Commit

---
+++ SimiliarityTesting/Program.cs
@@ -0,0 +1,72 @@
+﻿using F23.StringSimilarity;
+using SimiliarityStringTestingConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTPTestingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Damerau damerauTest = new Damerau();
+                //double SimiliarityAccuracy = damerauTest.Distance("Color", "Colour");
+                //Console.WriteLine("Result Accuracy gan: " + SimiliarityAccuracy+"%");
+
+                //F23.StringSimilarity.Levenshtein levenshteinTest = new F23.StringSimilarity.Levenshtein();
+                //double SimiliarityAccuracy2= levenshteinTest.Distance("Color", "Colour");
+                //Console.WriteLine("Result Accuracy gan: " + SimiliarityAccuracy2 + "%");
+
+                //var weightLevenshtein = new WeightedLevenshtein(new ExampleCharSub());
+                //double SimiliarityAccuracy3 = weightLevenshtein.Distance("Color", "Colour");
+                //Console.WriteLine("Result Accuracy gan: " + SimiliarityAccuracy3 + "%");
+
+                var ngram = new NGram(100);
+                double SimiliarityAccuracy5 = ngram.Distance("Color", "Colour") * 100;
+                Console.WriteLine("Result Accuracy gan: " + SimiliarityAccuracy5 + "%");
+
+                var ngram2 = new NGram(100);
+                double SimiliarityAccuracy6 = ngram2.Distance("firman", "talon pt") * 100;
+                Console.WriteLine("Result Accuracy gan: " + SimiliarityAccuracy6 + "%");
+
+                var jw = new JaroWinkler();
+
+                double SimiliarityAccuracy7 = jw.Similarity("firman", "talon pt") * 100;
+                Console.WriteLine("Result Accuracy gan: " + SimiliarityAccuracy7 + "%");
+
+                
+                double SimiliarityAccuracy8 = jw.Similarity("hendi", "firman hendi") * 100;
+                Console.WriteLine("Result Accuracy gan: " + SimiliarityAccuracy8 + "%");
+
+                double SimiliarityAccuracy10 = jw.Similarity("firman hendi setiawan", "setiawan hendi firman udin") * 100;
+                Console.WriteLine("Result Accuracy gan: " + SimiliarityAccuracy10 + "%");
+
+                double SimiliarityAccuracy9 = jw.Similarity("budi setiawan", "setiawan budi") * 100;
+                Console.WriteLine("Result Accuracy gan: " + SimiliarityAccuracy9 + "%");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+
+
+        private class ExampleCharSub : ICharacterSubstitution
+        {
+            public double Cost(char c1, char c2)
+            {
+                // The cost for substituting 't' and 'r' is considered smaller as these 2 are located next to each other on a keyboard
+                if (c1 == 't' && c2 == 'r') return 0.5;
+
+                // For most cases, the cost of substituting 2 characters is 1.0
+                return 1.0;
+            }
+        }
+    }
+}
---
+++ SimiliarityTesting/SimiliarityHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using Renci.SshNet;
+using System.Text.RegularExpressions;
+using F23.StringSimilarity.Interfaces;
+
+namespace SimiliarityStringTestingConsole
+{
+
+
+    public class Levenshtein : IMetricStringDistance
+    {
+
+        public double Distance(string s1, string s2)
+        {
+            return Distance(s1, s2, int.MaxValue);
+        }
+
+        public double Distance(string s1, string s2, int limit)
+        {
+            if (s1 == null)
+            {
+                throw new ArgumentNullException(nameof(s1));
+            }
+
+            if (s2 == null)
+            {
+                throw new ArgumentNullException(nameof(s2));
+            }
+
+            if (s1.Equals(s2))
+            {
+                return 0;
+            }
+
+            if (s1.Length == 0)
+            {
+                return s2.Length;
+            }
+
+            if (s2.Length == 0)
+            {
+                return s1.Length;
+            }
+
+            // create two work vectors of integer distances
+            int[] v0 = new int[s2.Length + 1];
+            int[] v1 = new int[s2.Length + 1];
+            int[] vtemp;
+
+            // initialize v0 (the previous row of distances)
+            // this row is A[0][i]: edit distance for an empty s
+            // the distance is just the number of characters to delete from t
+            for (int i = 0; i < v0.Length; i++)
+            {
+                v0[i] = i;
+            }
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                // calculate v1 (current row distances) from the previous row v0
+                // first element of v1 is A[i+1][0]
+                //   edit distance is delete (i+1) chars from s to match empty t
+                v1[0] = i + 1;
+
+                int minv1 = v1[0];
+
+                // use formula to fill in the rest of the row
+                for (int j = 0; j < s2.Length; j++)
+                {
+                    int cost = 1;
+                    if (s1[i] == s2[j])
+                    {
+                        cost = 0;
+                    }
+                    v1[j + 1] = Math.Min(
+                            v1[j] + 1,              // Cost of insertion
+                            Math.Min(
+                                    v0[j + 1] + 1,  // Cost of remove
+                                    v0[j] + cost)); // Cost of substitution
+
+                    minv1 = Math.Min(minv1, v1[j + 1]);
+                }
+
+                if (minv1 >= limit)
+                {
+                    return limit;
+                }
+
+                // copy v1 (current row) to v0 (previous row) for next iteration
+                // System.arraycopy(v1, 0, v0, 0, v0.length);
+
+                // Flip references to current and previous row
+                vtemp = v0;
+                v0 = v1;
+                v1 = vtemp;
+            }
+
+            return v0[s2.Length];
+        }
+
+    }
+}
